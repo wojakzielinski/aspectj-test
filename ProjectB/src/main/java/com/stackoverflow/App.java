917f6cface4e96aeafff8e79cbe3f813371c814a
Santhosh Kumar <brsanthu@gmail.com>
2018-10-04 15:05:20 -0700

    Initial commit

---
+++ ProjectB/src/main/java/com/stackoverflow/App.java
@@ -0,0 +1,15 @@
+package com.stackoverflow;
+
+/**
+ * @author maba, 2012-09-17
+ */
+public class App {
+
+    public void hello(String name) {
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.hello("world");
+    }
+}
